9b884e4e0ebf314acbbba01604837b63a1d84548
kozjava <andrei.drazdou@gmail.com>
2018-05-20 15:19:02 +0300

    small fixes

--- app/src/main/java/com/example/kozjava/smartalarm/settings/Config.kt
+++ app/src/main/java/com/example/kozjava/smartalarm/settings/Config.kt
@@ -7,7 +7,7 @@ class Config {
 
     companion object {
         fun getServerAddress() : String{
-            return "http://192.168.43.84:3000"
+            return "http://gsmserver.herokuapp.com"
         }
     }
 }
